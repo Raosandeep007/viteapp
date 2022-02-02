9602fe2c379e285a46ef30b815f7198ea8a39a41
Sandeep Yadav <yadavsandeep775@gmail.com>
2022-02-02 21:09:00 +0530

    tried

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { UserDetails } from './components/UserDetails'
-import { Counter } from './components/Counter'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { UserDetails } from "./components/UserDetails";
+import { Counter } from "./components/Counter";
 
 function App() {
   return (
-      <div className="App">
-      <UserDetails name="Sandeep" add = "delhi" age="23" isMarried={true}/>
-      <Counter/>
-      <Counter/>
-      </div>
+    <div className="App">
+      <UserDetails name="Rahul" add="delhi" age="25" isMarried={false} />
+      <Counter />
+      <Counter />
+    </div>
   );
 }
 
-export default App
+export default App;
